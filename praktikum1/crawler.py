54ca01a8f6af197d429721658756fb37057fb9e2
Tobias Rohrer <tobias.rohrer@outlook.com>
2019-11-12 11:47:04 +0100

    added link extraction for t3n/news

--- praktikum1/crawler.py
+++ praktikum1/crawler.py
@@ -2,17 +2,25 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.crawler import CrawlerProcess
 
+
 class HeiseSpider(scrapy.Spider):
     name = 'heise_spider'
     allowed_domains = ['t3n.de']
-    start_urls = ['https://t3n.de/news/expertenanhoerung-huawei-5g-1219846/']
+    start_urls = ['https://t3n.de/news']
 
     def parse(self, response):
+        extractor = LinkExtractor(allow='news', allow_domains=self.allowed_domains)
+        links = extractor.extract_links(response)
+
+        for link in links:
+            absolute_next_page_url = response.urljoin(link.url)
+            yield scrapy.Request(absolute_next_page_url)
+
         headings = response.xpath("//h2[@class='u-gap-medium u-text-extralarge']").extract()
         yield {'headings': headings}
 
 
-def runCrawler(name):
+def run_crawler(name):
     c = CrawlerProcess({
         'USER_AGENT': 'HochschuleDarmstadt-TextWebMining',
         'FEED_FORMAT': 'csv',
@@ -24,4 +32,5 @@ def runCrawler(name):
     c.crawl(HeiseSpider)
     c.start()  # the script will block here until the crawling is finished
 
-runCrawler('HeiseSpider')
+
+run_crawler('HeiseSpider')
